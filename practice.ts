3ed5e05312586c7af4cea5bab7a734b8d726d10d
Zayaan Siddiqui <zayaansuhail@gmail.com>
2026-01-28 18:11:30 -0500

    basic ts

---
+++ practice.ts
@@ -0,0 +1,47 @@
+function main(): void {
+    console.log("Hello, World!");
+    
+    // variables
+    let x: number = 42;
+    let str: string = "The answer is " + x;
+    let ok: boolean = true;
+//arrays 
+    let nums : number[] = [1,2,3,4,5];
+    let strs : string[] = ["string1", "string2"];
+    let bools : boolean[] = [true, false, true];
+
+    console.log(str);
+    console.log("Numbers:", nums);
+    console.log("Strings:", strs);
+    console.log("Booleans:", bools);
+}
+
+
+function add(a:number, b:number): number {
+    return (a + b);
+}
+
+const freq = new Map<string,number>();
+
+freq.set ("a",1);
+const cnt = (freq.get(leftChar) ?? 0) - 1;
+if(cnt === 0) freq.delete(leftChar);
+    else freq.set(leftChar, cnt);
+
+
+const seen = new Set<number>();
+seen.add(1);
+seen.has(1); 
+seen.delete(1);
+
+for (let i = 0; i < n; i++){}
+
+if (x === 0) {}
+
+for (const ch of "abc") {
+    console.log(ch);
+}
+
+node?.next
+
+
